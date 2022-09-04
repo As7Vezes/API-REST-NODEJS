fcd874a5ebde92a12b128365b09428e25d932a9c
As7Vezes <denysn60@gmail.com>
2022-09-03 22:18:15 -0300

    Adicionando ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log(1 + 1)
+import express, { NextFunction, Request, Response } from 'express'
+
+const app = express()
+
+const PORT = 8282
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'bar'})
+})
+
+app.listen(PORT, () => {
+    console.log('servidor rodando')
+})
